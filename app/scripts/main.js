5de8436cd0d719f597d05afcd9f22da271c1a6f2
Fellipe Barros <fellipe1609@gmail.com>
2017-12-22 02:10:29 -0200

    update slider - add many sliders

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,96 +1,95 @@
 "use strict";
 
-(function(){
+window.fbSlider = (elm) => {
     let 
-        elements = document.getElementsByClassName('fb-slider'),
-        options = undefined,
-        properties = [],
-        styles = [],
-        content = undefined,
-        current = undefined,
-        transition = 0,
-        timeout = undefined,
-        stop = undefined
-    
-    console.log( elements )
-
-    let
-        getOptions = (element) => {
-            options = JSON.parse(element.getAttribute('data-options'))
-
-            console.log('fb-slider => options', options)
-        },
-        getProperties = (current) => {
-            properties.width = current.clientWidth
-            properties.height = current.clientHeight
-
-            console.log('fb-slider => properties', options, properties)
-        },
-        setStyles = (current) => {
-            content = current.getElementsByClassName('fb-slider-content')[0]
-            
+        elements = elm,
+        startSlider = (elm) => {
             let 
-                qtd = content.getElementsByClassName('fb-item').length,
-                contentWidth = (properties.width * qtd) + 'px',
-                items = content.getElementsByClassName('fb-item'),
-                setElmStyle = (elm) => {
-                    elm.style.width = properties.width + 'px'
+                options = undefined,
+                properties = [],
+                styles = [],
+                content = undefined,
+                current = elm,
+                transition = 0,
+                timeout = undefined,
+                stop = undefined,
+                getOptions = () => {
+                    options = JSON.parse(current.getAttribute('data-options'))
 
-                    console.log('fb-slider => setStyle', elm)
+                    console.log('fb-slider => options', options)
+                },
+                getProperties = () => {
+                    properties.width = current.clientWidth
+                    properties.height = current.clientHeight
+
+                    console.log('fb-slider => properties', options, properties)
+                },
+                setStyles = () => {
+                    content = current.getElementsByClassName('fb-slider-content')[0]
+                    
+                    let 
+                        qtd = content.getElementsByClassName('fb-item').length,
+                        contentWidth = (properties.width * qtd) + 'px',
+                        items = content.getElementsByClassName('fb-item'),
+                        setElmStyle = (elm) => {
+                            elm.style.width = properties.width + 'px'
+
+                            console.log('fb-slider => setStyle', elm)
+                        }
+                    
+                    for(var i=0; i < items.length; i++) {
+                        setElmStyle(items[i])
+                    }
+
+                    content.style.width = contentWidth
+                    
+                    console.log('fb-slider => styles', content, qtd, contentWidth, items)
+                },
+                setAnimate = () => {
+                    setTimeout(function(){
+                        if( !stop ) {
+                            transition -= properties.width
+
+                            if( (transition * -1) === content.clientWidth )
+                                transition = 0
+
+                            content.style.transform = "translateX("+ transition +"px)"
+                        }                    
+
+                        setAnimate()
+                    }, options.transition)
+
+                    console.log('fb-slider => animate')
+                },
+                stopWhenOver = () => {
+                    let fn = () => {
+                        stop = true
+
+                        console.log( 'fb-slider => mouseenter' )
+                    }
+                    current.addEventListener("mouseenter", fn)
+                },
+                startWhenOverOut = () => {
+                    let fn = () => {
+                        stop = false
+
+                        console.log( 'fb-slider => mouseout' )
+                    }
+                    current.addEventListener("mouseout", fn)
                 }
-            
-            for(var i=0; i < items.length; i++) {
-                setElmStyle(items[i])
-            }
 
-            content.style.width = contentWidth
-            
-            console.log('fb-slider => styles', content, qtd, contentWidth, items)
-        },
-        setAnimate = (current) => {
-            setTimeout(function(){
-                if( !stop ) {
-                    transition -= properties.width
-
-                    if( (transition * -1) === current.clientWidth )
-                        transition = 0
-
-                    current.style.transform = "translateX("+ transition +"px)"
-                }                    
-
-                setAnimate(current)
-            }, options.transition)
-
-            console.log('fb-slider => animate')
-        },
-        stopWhenOver = (current) => {
-            let fn = () => {
-                stop = true
-
-                console.log( 'fb-slider => mouseenter' )
-            }
-            current.addEventListener("mouseenter", fn)
-        },
-        startWhenOverOut = () => {
-            let fn = () => {
-                stop = false
-
-                console.log( 'fb-slider => mouseout' )
-            }
-            current.addEventListener("mouseout", fn)
-        },
-        startSlider = () => {
-            // temp
-            current = elements[0]
-            // - temp
-
-            getOptions(current)
-            getProperties(current)
-            setStyles(current)
-            setAnimate(content)
-            stopWhenOver(current)
-            startWhenOverOut(current)
+            getOptions()
+            getProperties()
+            setStyles()
+            setAnimate()
+            stopWhenOver()
+            startWhenOverOut()
         }
 
-        startSlider()
+        for(var i=0; i < elements.length; i++)
+            startSlider(elements[i])
+}
+
+(function(){
+    new fbSlider(document.getElementsByClassName('myslider'))    
 })()
